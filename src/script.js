428776565306bbe85f1312fef8427642d15b7713
Li Zheng <>
2020-04-25 05:35:52 -0700

    Add layer size sync and flow creation toggles

--- src/script.js
+++ src/script.js
@@ -13,20 +13,36 @@ const layerType = {
 const overrideProperty = {
   IMAGE: "image",
 };
-let exportOptions = {
-  scales: Settings.settingForKey("image-scales"),
-  formats: "png",
-  output: false,
+const preferences = {
+  EXPORT_OPTIONS: {
+    scales: Settings.settingForKey("image-scales"),
+    formats: "png",
+    output: false,
+  },
+  IS_FLOW_ON: Settings.settingForKey("isFlowOn"),
+  IS_SIZESYNC_ON: Settings.settingForKey("isSizeSyncOn"),
 };
-
 let shapeCounter = 0;
 let symbolCounter = 0;
 
+export function syncLayerSize(targetLayer, sourceArtboard) {
+  targetLayer.frame.width = sourceArtboard.frame.width;
+  targetLayer.frame.height = sourceArtboard.frame.height;
+}
+
+export function updateFlowToSourceArtboard(targetLayer, sourceArtboard) {
+  if (preferences.IS_FLOW_ON) {
+    targetLayer.flow = { target: sourceArtboard };
+  } else {
+    targetLayer.flow = undefined;
+  }
+}
+
 export default function () {
-  if (selectedLayers.length < 1) {
+  if (selectedLayers.length === 0) {
     // -----------------------------------
     // Display a message when no layers are selected
-    UI.message("❌ Please select 1 artboard.");
+    UI.message("❌ Please select at least 1 artboard.");
   } else {
     selectedLayers.forEach((layer) => {
       if (layer.type != layerType.ARTBOARD) {
@@ -35,20 +51,20 @@ export default function () {
         UI.message("❌ Only content in artboards will be used for updating.");
       } else {
         const id = layer.id;
-        const buffer = sketch.export(layer, exportOptions);
+        const buffer = sketch.export(layer, preferences.EXPORT_OPTIONS);
         const newImg = sketch.createLayerFromData(buffer, "bitmap");
         const targetLayers = doc.getLayersNamed(layer.name);
 
         targetLayers.forEach((imglayer) => {
           if (id != imglayer.id && imglayer.type != layerType.ARTBOARD) {
-            imglayer.frame.width = layer.frame.width;
-            imglayer.frame.height = layer.frame.height;
+            if (preferences.IS_SIZESYNC_ON) syncLayerSize(imglayer, layer);
+            updateFlowToSourceArtboard(imglayer, layer);
 
             if (imglayer.type == layerType.SHAPEPATH) {
               imglayer.style.fills = [
                 {
                   pattern: {
-                    patternType: Style.PatternFillType.Fill,
+                    patternType: Style.PatternFillType.Fit,
                     image: newImg.image,
                   },
                   fillType: Style.FillType.pattern,
@@ -69,7 +85,9 @@ export default function () {
         });
 
         UI.message(
-          `✅ ${shapeCounter} layer(s) & ${symbolCounter} symbol(s) updated.`
+          `✅ ${shapeCounter} layer(s) & ${symbolCounter} symbol(s) updated at scale of ${Settings.settingForKey(
+            "image-scales"
+          )}`
         );
       }
     });
--- src/settings.js
+++ src/settings.js
@@ -2,22 +2,143 @@ let sketch = require("sketch");
 let Settings = sketch.Settings;
 let UI = sketch.UI;
 
-export default function () {
-  UI.getInputFromUser(
-    "Update filler image scale",
-    {
-      type: UI.INPUT_TYPE.selection,
-      initialValue: Settings.settingForKey("image-scales"),
-      possibleValues: ["1", "2", "3"],
-    },
-    (err, value) => {
-      Settings.setSettingForKey("image-scales", value);
-      let imageScales = Settings.settingForKey("image-scales");
-      UI.message(`✅ Image Scale is updated to ${imageScales}`);
-      if (err) {
-        // most likely the user canceled the input
-        return;
-      }
-    }
+const responseOptions = {
+  SAVE: 1000,
+  RESET: 1001,
+  CANCEL: 1002,
+};
+
+const imageScaleOptions = ["1", "2", "3"];
+
+const prefernceKey = {
+  IMAGE_SCALE: "image-scales",
+  IS_FLOW_ON: "isFlowOn",
+  IS_SIZESYNC_ON: "isSizeSyncOn",
+};
+
+const panelSpec = {
+  width: 300,
+  height: 160,
+  toggleFrame: 25,
+};
+
+let UIComponentRect = (y) =>
+  NSMakeRect(0, panelSpec.height - y, panelSpec.width, panelSpec.toggleFrame);
+
+let layerSizeSyncToggle, flowCreationToggle, imageScaleDropdown;
+
+function createLabel(positionY, text) {
+  const label = NSTextField.alloc().initWithFrame(UIComponentRect(positionY));
+  label.setStringValue(text);
+  label.setSelectable(false);
+  label.setEditable(false);
+  label.setBezeled(false);
+  label.setDrawsBackground(false);
+
+  return label;
+}
+
+function createToggle(positionY, settingKey, text) {
+  const toggle = NSButton.alloc().initWithFrame(UIComponentRect(positionY));
+  const initValue =
+    Settings.settingForKey(settingKey) == 0 ? NSOffState : NSOnState;
+  toggle.setButtonType(NSSwitchButton);
+  toggle.setBezelStyle(0);
+  toggle.setTitle(text);
+  toggle.setState(initValue);
+
+  return toggle;
+}
+
+function createDropdown(positionY, possibleValue, initValue) {
+  const dropdowm = NSPopUpButton.alloc().initWithFrame(
+    UIComponentRect(positionY)
   );
+  const initialIndex = possibleValue.indexOf(initValue);
+  dropdowm.addItemsWithTitles(possibleValue);
+  dropdowm.selectItemAtIndex(initialIndex !== -1 ? initialIndex : 0);
+
+  return dropdowm;
+}
+
+export function createSettingPanel() {
+  var panel = COSAlertWindow.new();
+  panel.setIcon(__command.pluginBundle().alertIcon());
+  panel.setMessageText("Fill Updater Settings");
+  panel.addButtonWithTitle("Save");
+  panel.addButtonWithTitle("Reset");
+  panel.addButtonWithTitle("Cancel");
+  const view = NSView.alloc().initWithFrame(
+    NSMakeRect(0, 0, panelSpec.width, panelSpec.height)
+  );
+  panel.addAccessoryView(view);
+
+  // -----------------------------------
+  // Image scale dropdown
+  let imageScaleLabel = createLabel(30, "Image scale:");
+  imageScaleDropdown = createDropdown(
+    50,
+    imageScaleOptions,
+    Settings.settingForKey(prefernceKey.IMAGE_SCALE)
+  );
+
+  // -----------------------------------
+  // Layer size sync and flow creation toggles
+  let toggleLabel = createLabel(100, "Other settings:");
+  layerSizeSyncToggle = createToggle(
+    120,
+    prefernceKey.IS_SIZESYNC_ON,
+    "Sync layer size"
+  );
+  flowCreationToggle = createToggle(
+    145,
+    prefernceKey.IS_FLOW_ON,
+    "Toggle flows"
+  );
+
+  view.addSubview(imageScaleLabel);
+  view.addSubview(imageScaleDropdown);
+
+  view.addSubview(toggleLabel);
+  view.addSubview(layerSizeSyncToggle);
+  view.addSubview(flowCreationToggle);
+
+  return panel.runModal();
+}
+
+export function resetSettings() {
+  Settings.setSettingForKey(prefernceKey.IMAGE_SCALE, imageScaleOptions[0]);
+  Settings.setSettingForKey(prefernceKey.IS_SIZESYNC_ON, true);
+  Settings.setSettingForKey(prefernceKey.IS_FLOW_ON, true);
+  UI.message(`✅ Successfully updated`);
+}
+
+export function updateSettings() {
+  const imageScale =
+    imageScaleOptions[imageScaleDropdown.indexOfSelectedItem()];
+  Settings.setSettingForKey(prefernceKey.IMAGE_SCALE, imageScale);
+  Settings.setSettingForKey(
+    prefernceKey.IS_SIZESYNC_ON,
+    layerSizeSyncToggle.state()
+  );
+  Settings.setSettingForKey(
+    prefernceKey.IS_FLOW_ON,
+    flowCreationToggle.state()
+  );
+  UI.message(`✅ Successfully updated`);
+}
+
+export default function () {
+  let response = createSettingPanel();
+  switch (response) {
+    case responseOptions.SAVE:
+      updateSettings();
+      break;
+    case responseOptions.RESET:
+      resetSettings();
+      break;
+    case responseOptions.CANCEL:
+      break;
+    default:
+  }
 }
